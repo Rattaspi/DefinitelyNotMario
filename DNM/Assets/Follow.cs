8b6e34c21b411e83d62ff72a769bdde583806db4
Alex Canut <a.canutperdigon@gmail.com>
2018-05-05 00:53:37 +0200

    Añadida escena de test

--- DNM/Assets/Follow.cs
+++ DNM/Assets/Follow.cs
@@ -7,6 +7,7 @@ public class Follow : MonoBehaviour {
     [SerializeField] private float offsetX = 0, offsetY = 0;
     [HideInInspector] public  float extraX = 0, extraY = 0; //estos offsets los controlan los triggers que varian el offset de la camara.
     private Vector3 speed; //variable velocidad de la camara para hacer el SmoothDamp()
+    private float interpolatedOffset;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +22,12 @@ public class Follow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        interpolatedOffset = Mathf.Lerp(transform.position.y, offsetY + extraY, 0.1f);
+        print("interpolated "+interpolatedOffset);
+        print("extra " + extraY);
+        if(Mathf.Abs(interpolatedOffset) < 0.3f) {
+            interpolatedOffset = extraY;
+        }
         transform.position = new Vector3(target.transform.position.x + offsetX + extraX, offsetY + extraY, -10);
 	}
 }
