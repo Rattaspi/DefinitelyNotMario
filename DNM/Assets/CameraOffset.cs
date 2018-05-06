68936e40ec6c3147bee2a35b159b6bcbe1740c07
Alex Canut <a.canutperdigon@gmail.com>
2018-05-07 00:51:24 +0200

    Funciona más o menos el smooth en la Y de la cámara.

--- DNM/Assets/CameraOffset.cs
+++ DNM/Assets/CameraOffset.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraOffset : MonoBehaviour {
-    [SerializeField] private Follow camera;
+    private Follow camera;
     [SerializeField] private float x = 0, y = 0;
 
     private void Start() {
-        //camera = FindObjectOfType<Follow>();
+        camera = FindObjectOfType<Follow>();
     }
 
     private void ChangeCameraOffset() {
-        print("change called");
         camera.extraX = x;
         camera.extraY = y;
     }
--- DNM/Assets/Follow.cs
+++ DNM/Assets/Follow.cs
@@ -6,7 +6,6 @@ public class Follow : MonoBehaviour {
     [SerializeField] private GameObject target;
     [SerializeField] private float offsetX = 0, offsetY = 0;
     [HideInInspector] public  float extraX = 0, extraY = 0; //estos offsets los controlan los triggers que varian el offset de la camara.
-    private Vector3 speed; //variable velocidad de la camara para hacer el SmoothDamp()
     private float interpolatedOffset;
 
 	// Use this for initialization
@@ -14,20 +13,17 @@ public class Follow : MonoBehaviour {
         transform.position = target.transform.position;
         Vector3 pos = transform.position;
         pos.x += (offsetX + extraX);
-        pos.y = pos.y + offsetY + extraY;
+        pos.y = offsetY;
         pos.z = -10;
         transform.position = pos;
-        speed = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        interpolatedOffset = Mathf.Lerp(transform.position.y, offsetY + extraY, 0.1f);
-        print("interpolated "+interpolatedOffset);
-        print("extra " + extraY);
-        if(Mathf.Abs(interpolatedOffset) < 0.3f) {
+        interpolatedOffset = Mathf.Lerp(transform.position.y, offsetY + extraY, 0.2f);
+        if (Mathf.Abs(transform.position.y - (offsetY+extraY)) < 0.2f) {
             interpolatedOffset = extraY;
         }
-        transform.position = new Vector3(target.transform.position.x + offsetX + extraX, offsetY + extraY, -10);
+        transform.position = new Vector3(target.transform.position.x + offsetX + extraX, offsetY + interpolatedOffset, -10);
 	}
 }
